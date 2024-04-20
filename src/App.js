c47673b83541005c6da9dc300e0e62fa40943198
TienVu <moameicute25702@gmail.com>
2024-04-20 13:04:39 +0700

    Add-Npx-Reactjs

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <p>
+        Hello ae
+      </p>
+      <h1>
+        One: Tien
+      </h1>
+      <h1>
+        Two: 
+      </h1>
+    </div>
+  );
+}
+
+export default App;
